66e850cc7f9c76b00303fcc6f76de8e599e1346c
kumoprogit@kumopro.com <Kumopro_0001>
2024-07-28 15:31:24 +0900

    First Commit

---
+++ src/forward.js
@@ -0,0 +1,114 @@
+var net = require("net");
+
+function forward(localAddress, localPort, destHost, destPort) {
+/*
+    if (args.length !== 2) {
+      return show_usage();
+    }
+*/  
+/*    let [localAddress, localPort] = split_host_port(host);
+    if (localPort.toString() !== split_host_port.port_s) {
+      return console.log('invalid port: ' + split_host_port.port_s);
+    }
+*/  
+/*    let [destHost, destPort] = split_host_port(target);
+    if (!destPort) {
+      return show_usage();
+    }
+    if (destPort.toString() !== split_host_port.port_s) {
+      return console.log('invalid port: ' + split_host_port.port_s);
+    }
+*/
+    destHost = destHost || 'localhost'
+  
+    console.log('Using parameters ' + JSON.stringify({localAddress, localPort, destHost, destPort}, null, '  '));
+  
+    let _l = 0; //log level.
+    let _pass_through = ((callback) => callback());
+    let _delay = 0;
+  
+    net.createServer({allowHalfOpen: true}, con => {
+      _l && console.log(`${getLogPrefix()}Connected from ${braceIpv6(con.remoteAddress)}:${con.remotePort}, now connecting to ${braceIpv6(destHost)}:${destPort}`);
+      const CON = net.connect({host: destHost, port: destPort}, () =>
+          _l && console.log(`${getLogPrefix()}Connected to ${braceIpv6(CON.remoteAddress)}:${CON.remotePort} src ${braceIpv6(CON.localAddress)}:${CON.localPort}`));
+  
+      [
+        {src: con, dst: CON, tag: `[${braceIpv6(con.remoteAddress)}:${con.remotePort} ---->> ${braceIpv6(destHost)}:${destPort}] `},
+        {src: CON, dst: con, tag: `[${braceIpv6(con.remoteAddress)}:${con.remotePort} <<---- ${braceIpv6(destHost)}:${destPort}] `}
+      ].forEach(v => {
+        v.src
+        .on('data', buf => {
+          _delay && (v.dotted = true, process.stderr.write('.'));
+          (_delay ? setTimeout : _pass_through)(() => {
+            v.dst.write(buf);
+            v.dotted && (v.dotted = false, process.stderr.write('\n'));
+            _l === 2 && process.stdout.write(prependHeadToEachLine(buf, Buffer.from(getLogPrefix() + v.tag)));
+          }, _delay);
+        })
+        .on('end', () => (v.dst.end(), _l && console.log(getLogPrefix() + v.tag + 'Ended')))
+        .on('close', () => (v.dst.destroy(), _l && console.log(getLogPrefix() + v.tag + 'Closed')))
+        .on('error', e => (v.dst.destroy(), _l && console.log(getLogPrefix() + v.tag + e)))
+      });
+    }).listen({host: localAddress, port: localPort}, function () {
+      const logLevelDescs = ['Disabled', 'Show connection', 'Show connection && Dump all in/out packets'];
+  
+      console.log(`Listening at ${braceIpv6(this.address().address)}:${this.address().port}`);
+      console.log(`Incoming connection will be forwarded to ${braceIpv6(destHost)}:${destPort}`);
+      console.log('You can press ENTER key to toggle Log level (default: 0 (none)).');
+      console.log('When log level is 0, you can input transmission delay in milliseconds.');
+  
+      //Optional: Read line from standard input to toggle log level
+      require('readline').createInterface({input: process.stdin}).on('line', line => {
+        line = line.replace(/\r?\n$/, '')
+        if (_l === 0 && line.match(/^\d{1,16}$/)) {
+          _delay = parseInt(line);
+          console.log(`Set transmission delay = ${_delay} ms`);
+        } else {
+          console.log('Log level : ' + logLevelDescs[(_l = (_l + 1) % logLevelDescs.length)]);
+          (_l === 0) && console.log('You can input transmission delay in milliseconds.');
+        }
+      });
+    }).on('error', e => console.log('' + e));
+  }
+  
+  function split_host_port(combo) {
+    let m = combo.match(/^(\d+)$|^\[([^\]]*)\]:?(.*)$|^([^:]*):([^:]*)$|^(.*)$/);
+    return [(m[2] || m[4] || m[6] || '').replace(/^\*$/, ''), (split_host_port.port_s = (m[1] || m[3] || m[5] || '')) & 0xffff];
+  }
+  
+  const NO_NEWLINE = Buffer.from('<NO_NEWLINE>\n');
+  
+  function prependHeadToEachLine(buf, headBuf) {
+    let buf_ary = [headBuf, buf];
+    for (; ;) {
+      let lastBuf = buf_ary.pop();
+      let nl_pos = lastBuf.indexOf(0xa); // find NEWLINE
+      if (nl_pos < 0) {
+        buf_ary.push(lastBuf);
+        buf_ary.push(NO_NEWLINE);
+        break;
+      } else if (nl_pos === lastBuf.length - 1) {
+        buf_ary.push(lastBuf);
+        break;
+      }
+      buf_ary.push(lastBuf.slice(0, nl_pos + 1)); // include NEWLINE
+      buf_ary.push(headBuf);
+      buf_ary.push(lastBuf.slice(nl_pos + 1));
+    }
+    return Buffer.concat(buf_ary);
+  }
+  
+  // prependHeadToEachLine(Buffer.from("a\nb\n"), Buffer.from("==> ")).toString()
+  
+  function braceIpv6(ip) {
+    if (ip.match(/^::ffff:\d+\.\d+\.\d+\.\d+$/)) {
+      return ip.slice(7)
+    }
+    return ip.indexOf(':') >= 0 ? `[${ip}]` : ip;
+  }
+  
+  function getLogPrefix() {
+    return (new Date()).toISOString().slice(11, 23) + ' ';
+  }
+  
+//  main(process.argv.slice(2)); //nodejs args is start from the 3rd.
---
+++ src/net.js
@@ -0,0 +1,92 @@
+//import { set_portforward } from "./sudo.js"
+
+const bodyParser = require("body-parser");
+const express = require('express');
+const http = express();
+const ip = require('ip');
+
+http.use(bodyParser.urlencoded({ extended: true }));
+//HTTPリクエストのボディをjsonで扱えるようになる
+http.use(bodyParser.json());
+
+const LISTENPORT = 80;
+const BASEPORT = 5000;
+const IPNUM = 10;
+const listen_address = new Array(IPNUM);
+var SELFIP = "";
+
+function server_listen() {
+    http.get('/regist', (req, res) => {
+        console.log("[Server]: Received from "+req.ip);
+        let no = this.add_address(req.ip);
+        let listen_port = BASEPORT+no;
+
+        if (no>=0) {
+            this.update();
+            res.json({
+                result: true,
+                ipaddr: listen_address[no],
+                port: listen_port
+            });
+        } else if (no == -1) {
+            res = "Existed.";
+        } else if (no == -2) {
+            res = "Overflowed.";
+        }
+    });
+    http.listen(LISTENPORT, '0.0.0.0', () => {
+        SELFIP = ip.address();
+        console.log('[Server]: Start. ' + SELFIP + ' listening on port '+LISTENPORT);
+        this.init();
+    });
+}
+
+
+function init() {
+    for (let i = 0; i < 10; i++) {
+        listen_address[i] = 0;
+    }
+}
+
+function add_address(ipaddr) {
+    var remoteAddress;
+    for (let i = 0; i < 10; i++) {
+        if (listen_address[i] == 0 ) continue;
+        if (ipaddr == listen_address[i]) {
+            return -1;
+        } 
+    }
+    for (let i = 0; i < 10; i++) {
+        if( listen_address[i] == 0 ) {
+            listen_address[i] = ipaddr;
+            set_portforward(BASEPORT+i, ipaddr, 9515);
+            //forward('localhost', String(BASEPORT+i), ipaddr, String(9515));
+            return i;
+        }
+    }
+    return -2;
+}
+function update() {
+    const tbl = document.getElementById('tbl');
+    const ipa = document.getElementById('self');
+    ipa.innerHTML = SELFIP;
+
+    if(tbl.rows.length>3) {
+    for(let i=tbl.rows.length;i>3;i--){
+//      let last = tbl.rows.length
+      tbl.deleteRow(-1);
+    }
+  }
+  for(let i=0;i<IPNUM;i++){
+    if ( listen_address[i] == 0 ) continue;
+    var row = tbl.insertRow(-1);
+    var cell1 = row.insertCell(-1);
+    var cell2 = row.insertCell(-1);
+    var cell3 = row.insertCell(-1);
+    var cell4 = row.insertCell(-1);
+    cell1.innerHTML = '127.0.0.1';
+    cell2.innerHTML = BASEPORT + i;
+    cell3.innerHTML = listen_address[i];
+    cell4.innerHTML = 9515;
+  }
+}
