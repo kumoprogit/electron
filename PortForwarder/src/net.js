0bcc2b2bdfab81fa48aba02b83d7fa2658825217
kumoprogit@kumopro.com <Kumopro_0001>
2024-08-05 21:03:31 +0900

    htmlで応答（途中）

--- PortForwarder/src/net.js
+++ PortForwarder/src/net.js
@@ -12,33 +12,39 @@ http.use(bodyParser.urlencoded({ extended: true }));
 //HTTPリクエストのボディをjsonで扱えるようになる
 http.use(bodyParser.json());
 
-const LISTENPORT = 3000;
-const BASEPORT = 5000;
-const IPNUM = 10;
-const listen_address = new Array(IPNUM);
-const device_address = new Array(IPNUM);
-var IPNO = 0;
+var data = {
+    listen_port: 3000,
+    base_port: 5000,
+    listen_address: [],
+    device_address: [],
+    ipno: 0
+};
 
 function server_listen() {
+//    http.use('/', express.static(__dirname));
+    http.set('view engine', 'ejs');
     http.get('/regist', (req, res) => {
         console.log("[Server]: Received from "+req.ip);
         let no = this.add_address(req.ip);
-        let listen_port = BASEPORT+no;
+        let listen_port = data.base_port+no;
 
         if (no>=0) {
             this.update();
+            res.render('./index.ejs', data);
+            /*
             res.json({
                 result: true,
                 ipaddr: listen_address[no],
                 port: listen_port
             });
+            */
         } else if (no == -1) {
             res = "Existed.";
         } else if (no == -2) {
             res = "Overflowed.";
         }
     });
-    http.listen(LISTENPORT, '0.0.0.0', () => {
+    http.listen(data.listen_port, '0.0.0.0', () => {
         this.init();
         var ifaces = os.networkInterfaces();
         let i = 0;
@@ -46,40 +52,41 @@ function server_listen() {
         Object.keys(ifaces).forEach(function (ifname) {
             ifaces[ifname].forEach(function (iface) {
                 if ("IPv4" === iface.family && iface.internal === false) {
-                    device_address[i] = iface.address;
+                    data.device_address[i] = iface.address;
                     i = i + 1;
                     console.log(iface);
                 }
             });
         });
         if (i > 1) {
-            ipcRenderer.send('select', device_address);
+            console.log(data.device_address);
+            ipcRenderer.send('select', data.device_address);
 
         }
-        console.log('[Server]: Start. ' + device_address[IPNO] + ' listening on port '+LISTENPORT);
+        console.log('[Server]: Start. ' + data.device_address[data.ipno] + ' listening on port '+data.listen_port);
     });
 }
 
 
 function init() {
     for (let i = 0; i < 10; i++) {
-        listen_address[i] = 0;
-        device_address[i] = 0;
+        data.listen_address[i] = 0;
+        data.device_address[i] = 0;
     }
 }
 
 function add_address(ipaddr) {
     var remoteAddress;
     for (let i = 0; i < 10; i++) {
-        if (listen_address[i] == 0 ) continue;
-        if (ipaddr == listen_address[i]) {
+        if (data.listen_address[i] == 0 ) continue;
+        if (ipaddr == data.listen_address[i]) {
             return -1;
         } 
     }
     for (let i = 0; i < 10; i++) {
-        if( listen_address[i] == 0 ) {
-            listen_address[i] = ipaddr;
-            set_portforward(BASEPORT+i, ipaddr, 9515);
+        if( data.listen_address[i] == 0 ) {
+            data.listen_address[i] = ipaddr;
+            set_portforward(data.base_port+i, ipaddr, 9515);
             //forward('localhost', String(BASEPORT+i), ipaddr, String(9515));
             return i;
         }
@@ -89,7 +96,7 @@ function add_address(ipaddr) {
 function update() {
     const tbl = document.getElementById('tbl');
     const ipa = document.getElementById('self');
-    ipa.innerHTML = device_address[IPNO] + ":" + LISTENPORT;
+    ipa.innerHTML = data.device_address[data.ipno] + ":" + data.listen_port;
 
     if(tbl.rows.length>3) {
     for(let i=tbl.rows.length;i>3;i--){
@@ -97,21 +104,21 @@ function update() {
       tbl.deleteRow(-1);
     }
   }
-  for(let i=0;i<IPNUM;i++){
-    if ( listen_address[i] == 0 ) continue;
+  for(let i=0;i<data.listen_address.length;i++){
+    if ( data.listen_address[i] == 0 ) continue;
     var row = tbl.insertRow(-1);
     var cell1 = row.insertCell(-1);
     var cell2 = row.insertCell(-1);
     var cell3 = row.insertCell(-1);
     var cell4 = row.insertCell(-1);
     cell1.innerHTML = '127.0.0.1';
-    cell2.innerHTML = BASEPORT + i;
-    cell3.innerHTML = listen_address[i];
+    cell2.innerHTML = data.base_port + i;
+    cell3.innerHTML = data.listen_address[i];
     cell4.innerHTML = 9515;
   }
 }
 ipcRenderer.on('select', (event,arg) => {
-    IPNO = arg;
+    data.ipno = arg;
     update();
     //console.log("arg=" + arg);
 });
